8aeadcb604f679e6565ebaf29089c5a921522602
ghost <noreply@localhost>
2023-04-02 16:07:57 +0300

    fix tags case

--- public/index.php
+++ public/index.php
@@ -22,8 +22,8 @@ $placeholder = Filter::plural($totalPages, [sprintf(_('Over %s page or enter the
     <title><?php echo _('YGGo! Web Search Engine') ?></title>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta name="Description" content="<?php echo _('Javascript-less Open Source Web Search Engine') ?>" />
-    <meta name="Keywords" content="<?php echo _('web, search, engine, crawler, php, pdo, sqlite, fts5, yggdrasil, js-less, open source') ?>" />
+    <meta name="description" content="<?php echo _('Javascript-less Open Source Web Search Engine') ?>" />
+    <meta name="keywords" content="<?php echo _('web, search, engine, crawler, php, pdo, sqlite, fts5, yggdrasil, js-less, open source') ?>" />
     <style>
 
       * {
