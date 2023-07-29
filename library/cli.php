c0922ef536dfe1caed7155e8090c3760d39f1887
ghost <localhost>
2023-07-29 17:22:25 +0300

    add CLI::brake method

--- library/cli.php
+++ library/cli.php
@@ -2,28 +2,33 @@
 
 class CLI {
 
+  public static function brake() {
+
+    echo PHP_EOL;
+  }
+
   public static function default(string $string) {
 
-    echo sprintf("%s", $string) . PHP_EOL;
+    echo sprintf("%s", $string) . self::brake();
   }
 
   public static function notice(string $string) {
 
-    echo sprintf("\033[36ms%s\033[0m", $string) . PHP_EOL;
+    echo sprintf("\033[36ms%s\033[0m", $string) . self::brake();
   }
 
   public static function warning(string $string) {
 
-    echo sprintf("\033[33m%s\033[0m", $string) . PHP_EOL;
+    echo sprintf("\033[33m%s\033[0m", $string) . self::brake();
   }
 
   public static function danger(string $string) {
 
-    echo sprintf("\033[31m%s\033[31m", $string) . PHP_EOL;
+    echo sprintf("\033[31m%s\033[31m", $string) . self::brake();
   }
 
   public static function success(string $string) {
 
-    echo sprintf("\033[32m%s\033[32m", $string) . PHP_EOL;
+    echo sprintf("\033[32m%s\033[32m", $string) . self::brake();
   }
 }
