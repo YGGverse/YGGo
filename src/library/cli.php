b2d7fb2fefb4ddac8fd869de759f626b932751ad
ghost <localhost>
2023-08-14 12:08:05 +0300

    fix line break return

--- src/library/cli.php
+++ src/library/cli.php
@@ -9,26 +9,36 @@ class CLI {
 
   public static function default(string $string) {
 
-    echo sprintf("%s", $string) . self::break();
+    echo sprintf("%s", $string);
+
+    self::break();
   }
 
   public static function notice(string $string) {
 
-    echo sprintf("\033[36m%s\033[0m", $string) . self::break();
+    echo sprintf("\033[36m%s\033[0m", $string);
+
+    self::break();
   }
 
   public static function warning(string $string) {
 
-    echo sprintf("\033[33m%s\033[0m", $string) . self::break();
+    echo sprintf("\033[33m%s\033[0m", $string);
+
+    self::break();
   }
 
   public static function danger(string $string) {
 
-    echo sprintf("\033[31m%s\033[0m", $string) . self::break();
+    echo sprintf("\033[31m%s\033[0m", $string);
+
+    self::break();
   }
 
   public static function success(string $string) {
 
-    echo sprintf("\033[32m%s\033[0m", $string) . self::break();
+    echo sprintf("\033[32m%s\033[0m", $string);
+
+    self::break();
   }
 }
